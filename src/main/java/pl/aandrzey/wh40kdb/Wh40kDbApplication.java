8bf11a06b8536e22a3285336a9be0e4770ea3f64
andrzej.hemer <Warhammer09>
2019-11-04 16:59:03 +0100

    db setup

---
+++ src/main/java/pl/aandrzey/wh40kdb/Wh40kDbApplication.java
@@ -0,0 +1,13 @@
+package pl.aandrzey.wh40kdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wh40kDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wh40kDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/aandrzey/wh40kdb/Wh40kDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.aandrzey.wh40kdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wh40kDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
